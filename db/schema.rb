fc8bf0a5871df7a8febe1d99861adf4452c8112c
jloman <jloman200@gmail.com>
2023-03-18 14:16:10 +0300

    Add devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_18_104755) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_18_105805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "text"
+    t.string "image"
+    t.integer "likes_count"
+    t.integer "comments_count"
+    t.integer "shares_count"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -38,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_18_104755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
